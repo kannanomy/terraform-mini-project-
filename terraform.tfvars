1b551ce1ce793d6be7cbac302ac582bdecd09fa6
root <root@ip-172-31-32-189.ap-south-1.compute.internal>
2026-04-14 11:54:55 +0000

    terraform project

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+ami_id        = "ami-079b5e5b3971bd10d"
+instance_type = "t2.micro"
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "ami_id" {
+  default = "ami-079b5e5b3971bd10d"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
